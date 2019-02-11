e66299bc756c3045703e3f038e9181a5d53f087b
mattboran <mattboran@gmail.com>
2019-02-11 13:24:04 -0500

    Getting type of material from maya

--- exportMaterials.py
+++ exportMaterials.py
@@ -9,29 +9,56 @@ import maya.OpenMayaMPx as OpenMayaMPx
 kPluginCmdName = "exportMaterials"
 kShortOutputFlagName = '-o'
 kLongOutputFlagName = '-output'
-baseOutputPath = "/home/matt/Documents/CudaRT/settings/"
+kBaseOutputMaterials = "/home/matt/Documents/CudaRT/meshes/%s"
+
+kNameLambert = "LAMBERT"
+kNameSpecular = "SPECULAR"
+kNameDiffspec = "DIFFSPEC"
+kNameGlossy = "MICROFACET"
+kNameRefractive = "REFRACTIVE"
 
 class exportMaterials(OpenMayaMPx.MPxCommand):
     def __init__(self):
         OpenMayaMPx.MPxCommand.__init__(self)
         
     def doIt(self, args):
-        matls = cmds.ls(mat=1)
-        materials = []
-        for mat in matls:
-            specs = {'name': mat}
-            specs['ka'] = self.getAttributeOrDefault(mat, "ambientColor", [0,0,0])
-            specs['kd'] = self.getAttributeOrDefault(mat, "color", [0.5,0.5,0.5])
-            specs['diffuse'] = self.getAttributeOrDefault(mat, "diffuse", 1.0)
-            specs['roughness'] = self.getAttributeOrDefault(mat, "roughness", 0.0)
-            specs['ks'] = self.getAttributeOrDefault(mat, "specularColor", [0.5, 0.5, 0.5])
-            specs['ni'] = self.getAttributeOrDefault(mat, "refractiveIndex", 1.0)
-            materials.append(specs)
-        output = {'materials': materials}
         outName = self.parseArguments(args)
-        outputPath = '%s%s-materials.json' % (baseOutputPath, outName)
-        with open(outputPath, 'w') as f:
-            f.write(json.dumps(output))
+        fileData = None
+        # TODO: Try except, with the following command
+        # cmds.file(force=True, options="groups=0;ptgroups=0;materials=1;smoothing=1;normals=1", type="OBJexport", pr=True, ea=(baseOutputMaterials % (outName+".obj")))
+        with open(kBaseOutputMaterials % ("%s.mtl" % outName)) as f:
+            fileData = f.read()
+        materials = fileData.split('newmtl ')[1:]
+        print "Got n=%s materials" % len(materials)
+        newProps = []
+        for material in materials:
+            props = material.split('\n')
+            if not props[0].endswith('SG'):
+                continue
+            sgName = props[0]
+            name = cmds.defaultNavigation(defaultTraversal=True, destination="%s.surfaceShader" % sgName)[0]
+            type = self.getMaterialType(name)
+            print "Material %s has type %s" % (name, type)
+            
+        
+    def getMaterialType(self, name):
+        matlType = cmds.objectType(name)
+        type = "NoneType"
+        if "lambert" in matlType:
+            return kNameLambert
+        if cmds.getAttr("%s.refractions" % name) == 1 and cmds.getAttr("%s.refractiveIndex" % name) != 1.0:
+            return kNameRefractive
+        ks = cmds.getAttr("%s.specularColor" % name)[0]
+        kd = cmds.getAttr("%s.color" % name)[0]
+        if kd == (0.0, 0.0, 0.0):
+            try:
+                if cmds.getAttr("%s.roughness" % name) > 0.0:
+                    return kNameGlossy
+            except ValueError:
+                pass
+            return kNameSpecular
+        return kNameDiffspec
+        
             
     def parseArguments(self, args):
         argData = OpenMaya.MArgParser(self.syntax(), args)
